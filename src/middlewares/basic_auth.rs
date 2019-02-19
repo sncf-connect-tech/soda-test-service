57f0c7e62ab25e4f6c655b7855b68dbfae72890b
theredfish <did.julian@gmail.com>
2019-02-19 13:56:53 +0100

    Remove trailing space

--- src/middlewares/basic_auth.rs
+++ src/middlewares/basic_auth.rs
@@ -12,7 +12,7 @@ impl Middleware<AppState> for Auth {
         let auth_user = &req.state().auth_user;
         let auth_pwd = &req.state().auth_pwd;
 
-        // If the username and the password are empty , we consider that the test service has
+        // If the username and the password are empty, we consider that the test service has
         // been configured to run without the Basic-Auth.
         if auth_user.is_empty() && auth_pwd.is_empty() {
             return Ok(Started::Done);
