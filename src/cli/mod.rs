d0d6e25cbc8615f9b7e6385f28facb289f061056
maxime_jeusselin <mjeusselin.prestataire@oui.sncf>
2019-08-23 15:13:41 +0200

    Update cli with clap

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,34 +1,29 @@
-use clap::{Arg, ArgMatches};
+use clap::{App, Arg, ArgMatches};
+
+fn validate_format(v: String) -> Result<(), String> {
+  if v.contains(':') {
+    return Ok(());
+  }
+  Err(String::from("Format must be IP:PORT"))
+}
 
 pub fn init<'a>() -> ArgMatches<'a> {
-    clap::App::new("HTTP Proxy")
-        .arg(
-            Arg::with_name("listen_addr")
-                .takes_value(true)
-                .value_name("LISTEN ADDR")
-                .index(1)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("listen_port")
-                .takes_value(true)
-                .value_name("LISTEN PORT")
-                .index(2)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("forward_addr")
-                .takes_value(true)
-                .value_name("FWD ADDR")
-                .index(3)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("forward_port")
-                .takes_value(true)
-                .value_name("FWD PORT")
-                .index(4)
-                .required(true),
-        )
-        .get_matches()
+  App::new("HTTP Proxy")
+    .arg(
+      Arg::with_name("listen")
+        .long("listen")
+        .help("format : IP:PORT")
+        .takes_value(true)
+        .validator(validate_format)
+        .required(true),
+    )
+    .arg(
+      Arg::with_name("forward")
+        .long("forward")
+        .help("format : IP:PORT")
+        .takes_value(true)
+        .validator(validate_format)
+        .required(true),
+    )
+    .get_matches()
 }
