193c3036dad53e9c2689264e1c9ef63903d4b5d4
ahery95 <amaury.hery@outlook.fr>
2020-01-08 10:25:13 +0100

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@
 extern crate serde_derive;
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate clap;
 
 use env_logger;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Error as HyperError, Server};
-
+use std::net::{SocketAddr, ToSocketAddrs};
+use url::Url;
+mod cli;
 mod domain;
 mod inspector;
 mod proxy;
@@ -15,7 +19,26 @@ mod proxy;
 async fn main() {
   std::env::set_var("RUST_LOG", "info");
   env_logger::init();
-  let in_addr = ([127, 0, 0, 1], 8081).into();
+  let matches = cli::init();
+
+  // Configure addresses to listen and forward.
+  let listen = matches.value_of("listen").unwrap();
+  let forwarded = matches.value_of("forward").unwrap();
+
+  // Used to give a more verbose output. (all info logs)
+  let verbose = matches.occurrences_of("verbose");
+
+  // Configure the timeout for the proxy, default to 60s
+  let timeout = value_t!(matches, "timeout", u32).unwrap_or(60);
+
+  // Verify and build the forward URL.
+  let forward_url = Url::parse(&format!(
+    "http://{}",
+    forwarded.to_socket_addrs().unwrap().next().unwrap()
+  ))
+  .unwrap();
+
+  let in_addr = ([127, 0, 0, 1], 8080).into();
 
   let make_svc = make_service_fn(|_conn| {
     async {
