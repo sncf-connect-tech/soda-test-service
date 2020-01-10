ce31cc59c834135aa81c8ee4b1b3caa720e9ff54
ahery95 <43137919+ahery95@users.noreply.github.com>
2020-01-10 18:22:46 +0100

    Update core architecture (#17)
    The verbose mode isn't implemented for now. Should
    be done later. Also, remove the Basic Auth.
    
    * Update architecture + Update session and inspector
    
    * LF to CRLF
    
    * Delete Makefile
    
    * Add CLI
    
    * Update cli and create client builder
    
    * Creation client in main with timeout
    
    * Add struct AppState with Client and Forward_url
    
    * Update Session and Struct AppState
    
    * Syntax modification to merge
    
    * Fix indentation
    
    Co-authored-by: Julian Didier <did.julian@gmail.com>

---
+++ src/domain/mod.rs
@@ -0,0 +1,7 @@
+mod capabilities;
+mod command;
+pub mod session;
+
+pub use self::capabilities::{Capabilities, DesiredCapabilities};
+pub use self::command::Command;
+pub use self::session::SessionStatus;
